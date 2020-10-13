d10f3b433588c5c8b738689815ff9ba70f111934
SeungYeon <yeondiary@gmail.com>
2020-10-13 23:38:02 +0900

    Solved week1_5

--- week1/05_target_number/target_number.cpp
+++ week1/05_target_number/target_number.cpp
@@ -5,10 +5,28 @@
 #include <iostream>
 
 using namespace std;
+int temp;
+
+void check(vector<int> numbers, int target, int idx, int sum) {
+    if(idx == numbers.size())
+    {
+        if(sum == target)
+            temp++;
+        
+        return;
+    }
+    
+    check(numbers, target, idx + 1, sum + numbers[idx]);
+    check(numbers, target, idx + 1, sum - numbers[idx]);
+}
 
 int solution(vector<int> numbers, int target) {
-	int answer = 0;
-	return answer;
+    int answer = 0;
+    
+    check(numbers, target, 0, 0);
+    answer = temp;
+    
+    return answer;
 }
 
 int main()
