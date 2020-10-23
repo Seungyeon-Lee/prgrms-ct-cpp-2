1eaaee218d14d8f5eadd595bfb7f45e3e3baa82d
SeungYeon <yeondiary@gmail.com>
2020-10-23 22:35:42 +0900

    피드백 반영

--- week1/04_kth_number/kth_number.cpp
+++ week1/04_kth_number/kth_number.cpp
@@ -10,15 +10,22 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
-    for(int i = 0; i<commands.size(); i++) {
-        vector <int> temp;
-        for(int j = commands[i][0] - 1; j<commands[i][1]; j++)
-            temp.push_back(array[j]);
+    for(int i = 0; i<commands.size(); i++)
+    {
+        int n1 = commands[i][0] - 1;
+        int n2 = commands[i][1];
+        int idx = 0;
         
-        sort(temp.begin(), temp.end());
+        vector<int> split_arr(array.begin() + n1, array.begin() + n2);
         
-        answer.push_back(temp[commands[i][2] - 1]);
+        for(int j = n1; j<n2; j++)
+            split_arr[idx++] = array[j];
+        
+        sort(split_arr.begin(), split_arr.end());
+        
+        answer.push_back(split_arr[commands[i][2] - 1]);
     }
+    
     return answer;
 }
 
