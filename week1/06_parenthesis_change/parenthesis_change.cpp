da56863e46a6bb854f2e9546fcdca103743c4ddf
SeungYeon <yeondiary@gmail.com>
2020-10-25 21:08:28 +0900

    피드백 반영

--- week1/06_parenthesis_change/parenthesis_change.cpp
+++ week1/06_parenthesis_change/parenthesis_change.cpp
@@ -6,70 +6,67 @@
 
 using namespace std;
 
-bool isPossible(string p){
-    int res = 0;
-    
-    for(int i = 0; i<p.size(); i++)
-    {
-        if(p[i] == '(')
-            res++;
-        else
-        {
-            res--;
-            if(res < 0)
-                return false;
-        }
-    }
-    
-    return true;
+bool isPossible(string p) {
+	int res = 0;
+
+	for (int i = 0; i < p.size(); i++)
+	{
+		if (p[i] == '(')
+			res++;
+		else
+		{
+			res--;
+			if (res < 0)
+				return false;
+		}
+	}
+
+	return true;
 }
 
 string makeCorrect(string p) {
-    if(p == "")
-        return p;
-    
-    string u = "", v = "";
-    int l = 0, r = 0;
-    
-    for(int i = 0; i<p.size(); i++)
-    {
-        if(p[i] == '(')
-            l++;
-        else
-            r++;
-        
-        if(l == r) {
-            u = p.substr(0, i + 1);
-            v = p.substr(i + 1);
-            break;
-        }
-    }
-    
-    if(isPossible(u))
-        return u + makeCorrect(v);
-    else
-    {
-        string w = "(";
-        w += makeCorrect(v) + ")";
-        u = u.substr(1, u.size() - 2);
-        
-        for(int i = 0; i<u.size(); i++)
-        {
-            if(u[i] == '(')
-                w += ')';
-            else
-                w += '(';
-        }
-        
-        return w;
-    }
+	if (p == "")
+		return p;
+
+	string u = "", v = "";
+	int l = 0, r = 0;
+
+	for (int i = 0; i < p.size(); i++)
+	{
+		if (p[i] == '(')
+			l++;
+		else
+			r++;
+
+		if (l == r) {
+			u = p.substr(0, i + 1);
+			v = p.substr(i + 1);
+			break;
+		}
+	}
+
+	if (isPossible(u))
+		return u + makeCorrect(v);
+	else
+	{
+		string w = "(";
+		w += makeCorrect(v) + ")";
+		string x = u.substr(1, u.size() - 2);
+
+		for (int i = 0; i < x.size(); i++)
+		{
+			if (x[i] == '(')
+				w += ')';
+			else
+				w += '(';
+		}
+
+		return w;
+	}
 }
 
 string solution(string p) {
-    if(isPossible(p))
-        return p;
-    else
-        return makeCorrect(p);
+	return makeCorrect(p);
 }
 
 int main()
