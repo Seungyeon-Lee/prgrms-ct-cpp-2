467be689d945aa0c70425072bca9551ae39eefc8
SeungYeon <yeondiary@gmail.com>
2020-10-25 22:27:38 +0900

    Solved week2_3

--- week2/03_disguise/disguise.cpp
+++ week2/03_disguise/disguise.cpp
@@ -2,13 +2,32 @@
 
 #include <string>
 #include <vector>
+#include <set>
 #include <iostream>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 0;
-    return answer;
+	int answer = 1;
+	set <string> types;
+
+	for (int i = 0; i < clothes.size(); i++)
+		types.insert(clothes[i][1]);
+
+	int sum = 0;
+	for (string type : types)
+	{
+		for (int k = 0; k < clothes.size(); k++)
+			if (clothes[k][1] == type)
+				sum++;
+
+		answer *= (sum + 1);
+		sum = 0;
+	}
+
+	answer--;
+
+	return answer;
 }
 
 int main()
