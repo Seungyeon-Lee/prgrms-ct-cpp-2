2c3b660f7e78bf5f92f95eaf7af452827d01b682
SeungYeon <yeondiary@gmail.com>
2020-10-26 18:41:07 +0900

    Solved week2_1

--- week2/01_function/function.cpp
+++ week2/01_function/function.cpp
@@ -8,6 +8,26 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
 	vector<int> answer;
+
+	int k = 1;
+	int cur = (100 - progresses[0]) / (float)speeds[0] + 0.9f;
+
+	for (int i = 1; i < progresses.size(); i++)
+	{
+		int next = (100 - progresses[i]) / (float)speeds[i] + 0.9f;
+
+		if (cur >= next)
+			k++;
+		else
+		{
+			answer.push_back(k);
+			k = 1;
+			cur = next;
+		}
+	}
+
+	answer.push_back(k);
+
 	return answer;
 }
 
