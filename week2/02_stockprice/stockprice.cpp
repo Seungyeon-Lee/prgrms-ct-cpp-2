34da7a04c301090af054f72f07e7738338bdb3ab
SeungYeon <yeondiary@gmail.com>
2020-10-26 19:05:43 +0900

    Solved week2_2

--- week2/02_stockprice/stockprice.cpp
+++ week2/02_stockprice/stockprice.cpp
@@ -7,8 +7,21 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-	vector<int> answer;
-	return answer;
+    vector<int> answer(prices.size(), 0);
+
+    for (int i = 0; i < prices.size(); i++)
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            if (prices[i] <= prices[j])
+                answer[i]++;
+            else
+            {
+                answer[i]++;
+                break;
+            }
+        }
+
+    return answer;
 }
 
 template<typename T>
