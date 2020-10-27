139b5b4f3e5d17e6b3ae3e782004d977662b353c
SeungYeon <yeondiary@gmail.com>
2020-10-27 13:36:12 +0900

    Solved week3_5

--- week3/05_word_change/word_change.cpp
+++ week3/05_word_change/word_change.cpp
@@ -2,12 +2,67 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
+bool visited[51];
+
+bool isPossible(string st, string ed) {
+
+	int n = 0;
+	for (int i = 0; i < st.size(); i++)
+	{
+		if (st[i] != ed[i])
+			n++;
+
+		if (n > 1)
+			return false;
+	}
+
+	return true;
+}
+
 int solution(string begin, string target, vector<string> words) {
 	int answer = 0;
+	bool flag = false;
+
+	for (int i = 0; i < words.size(); i++)
+		if (target == words[i])
+			flag = true;
+
+	if (!flag)
+		return 0;
+
+	queue <pair<string, int>> que;
+	for (int i = 0; i < words.size(); i++)
+		if (isPossible(begin, words[i]))
+		{
+			que.push({ words[i], 1 });
+			visited[i] = true;
+		}
+
+	while (!que.empty())
+	{
+		string st = que.front().first;
+		int n = que.front().second;
+		que.pop();
+
+		for (int i = 0; i < words.size(); i++)
+			if (isPossible(st, words[i]) && !visited[i])
+			{
+				que.push({ words[i], n + 1 });
+				visited[i] = true;
+			}
+
+		if (st == target)
+		{
+			answer = n;
+			break;
+		}
+	}
+
 	return answer;
 }
 
