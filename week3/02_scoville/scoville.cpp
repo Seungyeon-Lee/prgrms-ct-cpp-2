fa7fe99974cfa59224236efa5ec4626118aa81ff
SeungYeon <yeondiary@gmail.com>
2020-10-27 13:14:03 +0900

    Solved week3_2

--- week3/02_scoville/scoville.cpp
+++ week3/02_scoville/scoville.cpp
@@ -2,13 +2,34 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-	int answer = 0;
-	return answer;
+    int answer = 0;
+    priority_queue <int, vector<int>, greater<int>> pq;
+
+    for (int sc : scoville)
+        pq.push(sc);
+
+    while (pq.top() < K)
+    {
+        int first = pq.top();
+        pq.pop();
+
+        if (pq.empty())
+            return -1;
+
+        int second = pq.top();
+        pq.pop();
+
+        pq.push(first + (second * 2));
+        answer++;
+    }
+
+    return answer;
 }
 
 int main()
