30885754e6c3a1d87d2e740b64b6ac7cc3e206fb
SeungYeon <yeondiary@gmail.com>
2020-10-27 10:45:04 +0900

    나머지 연산자를 사용한 오류 수정

--- week2/01_function/function.cpp
+++ week2/01_function/function.cpp
@@ -10,11 +10,16 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
 	vector<int> answer;
 
 	int k = 1;
-	int cur = (100 - progresses[0]) / (float)speeds[0] + 0.9f;
+	int cur = (100 - progresses[0]) / speeds[0];
+	if ((100 - progresses[0]) % speeds[0] != 0)
+		cur++;
 
 	for (int i = 1; i < progresses.size(); i++)
 	{
-		int next = (100 - progresses[i]) / (float)speeds[i] + 0.9f;
+		int next = (100 - progresses[i]) / speeds[i];
+
+		if ((100 - progresses[i]) % speeds[i] != 0)
+			next++;
 
 		if (cur >= next)
 			k++;
