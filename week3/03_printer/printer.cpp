de6426a9ca84b827f56d3d62e8f843807c487cd7
SeungYeon <yeondiary@gmail.com>
2020-10-27 14:20:37 +0900

    Solved week3_3

--- week3/03_printer/printer.cpp
+++ week3/03_printer/printer.cpp
@@ -2,12 +2,45 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
 	int answer = 0;
+
+	queue <int> que;
+	priority_queue <int> pq;
+
+	for (int i = 0; i < priorities.size(); i++)
+	{
+		que.push(i);
+		pq.push(priorities[i]);
+	}
+
+	while (!que.empty())
+	{
+		int size = que.size();
+		while (size--)
+		{
+			int idx = que.front();
+			que.pop();
+			if (pq.top() == priorities[idx])
+			{
+				pq.pop();
+				answer++;
+
+				if (idx == location)
+					return answer;
+				else
+					continue;
+			}
+
+			que.push(idx);
+		}
+	}
+
 	return answer;
 }
 
