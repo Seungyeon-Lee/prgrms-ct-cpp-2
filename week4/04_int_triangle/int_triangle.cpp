ea3165937f5ec57a2d310e8f74798025afa35c50
SeungYeon <yeondiary@gmail.com>
2020-11-08 23:09:31 +0900

    Solved week4_4

--- week4/04_int_triangle/int_triangle.cpp
+++ week4/04_int_triangle/int_triangle.cpp
@@ -7,8 +7,27 @@
 using namespace std;
 
 int solution(vector<vector<int>> triangle) {
-	int answer = 0;
-	return answer;
+    int answer = 0;
+    int dp[501][501] = { 0, };
+    answer = dp[0][0] = triangle[0][0];
+
+    for (int i = 1; i < triangle.size(); i++)
+        for (int j = 0; j <= i; j++) 
+        {
+            // 왼쪽
+            if (j == 0)
+                dp[i][j] = dp[i - 1][j] + triangle[i][j];
+            // 오른쪽
+            else if (j == i)
+                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+            // 중간
+            else
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+
+            answer = max(answer, dp[i][j]);
+        }
+
+    return answer;
 }
 
 int main()
